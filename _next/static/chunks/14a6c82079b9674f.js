353d7b7cd96a55e51e82218e3293c661cbc9dd88
Muhammed Gayret <gayretmuhammed@gmail.com>
2026-08-24 23:48:19 +0300

    Site yayını

---
+++ _next/static/chunks/14a6c82079b9674f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/8080651723cc3a04.js",
+  "static/chunks/turbopack-1a0cef19f6c03440.js"
+])
---
+++ _next/static/chunks/56dfa768349459cc.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/8080651723cc3a04.js",
+  "static/chunks/turbopack-2c57fcd934746868.js"
+])
